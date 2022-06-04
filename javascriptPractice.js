d8a6d0b5b6854c8a4d82026968ca4b18a86fe275
Ashfaque Rahaman <nvjkvhdjkh@gmail.com>
2022-06-04 12:14:45 +0600

    first commit

---
+++ javascriptPractice.js
@@ -0,0 +1,66 @@
+// funtion or methodology to revesing a string or number
+
+// let str = "guiter";
+// console.log("The original string is " + str);
+// let splitStr = str.split("");
+// console.log("the split string is " + splitStr);
+// let reverseStr = splitStr.reverse();
+// console.log("the reverse string is " + reverseStr);
+// let joinStr = reverseStr.join("");
+// console.log("the join string is " + joinStr);
+
+// let number = 12345;
+
+// console.log(number.toString().split(""));
+// number splitting would be return as an array
+
+// now turning this number to the string
+// let numberToString = number + "";
+// console.log(numberToString);
+// console.log(number.toString().split("").reverse().join(""));
+// console.log(number.toString().split("").reverse().join(""));
+
+// const str2="guiter"
+
+// let reverseStr=""
+
+// for (let i=str2.length-1;i>=0;i--){
+//     reverseStr=reverseStr+str2[i]
+
+// }
+
+// console.log(reverseStr)
+
+// checking whether the string is palindrome or not
+// palindrome means if a string remainns same if you reverse it
+
+// var newStr="madama"
+// var splitStr2=newStr.split("")
+// var reverseStr2=splitStr2.reverse()
+// var joinStr=reverseStr2.join("")
+// function chackingPalidromeOrNot(){
+//     if(joinStr===newStr){
+//          console.log("The String is Palindrome")
+//     }else{
+//         console.log("The string is not Palindrome")
+//     }
+// }
+// checkingPalidromeOrNot()
+
+// const newArr = [6000, 1, 50, 2, 3, 4, 1000, 5, 6, 7, 8, 2000, 9, 500, 5000];
+
+// let largestNumber = newArr[0];
+// let secondLargestNumber = 0;
+
+// for (i = 1; i < newArr.length; i++) {
+//   if (newArr[i] > largestNumber) {
+//     secondLargestNumber = largestNumber;
+//     largestNumber = newArr[i];
+//   }
+
+//   if (newArr[i] !== largestNumber && newArr[i] > secondLargestNumber) {
+//     secondLargestNumber = newArr[i];
+//   }
+// }
+// console.log(largestNumber);
+// console.log(secondLargestNumber);
